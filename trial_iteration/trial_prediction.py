547d263d114c4e60bde4865d44936188ac8e39f0
“Stanislav” <ymedia432@ya.ru>
2023-04-10 22:47:54 +0500

    preliminary version, changed trial_prediction.py and Notebook

--- trial_iteration/trial_prediction.py
+++ trial_iteration/trial_prediction.py
@@ -12,12 +12,17 @@ PATH_LOAD_MODEL = os.path.abspath(os.path.join('..', *config['model']['path']))
 def __trial_predict_model_img_lr():
     """ Пробная итерация предсказания модели на тестовом изображении """
 
-    path_load_image = os.path.join('.', 'trial_image_MB.jpg')
+    path_load_image = os.path.join('.', 'trial_image_JR.jpg')
     predict_model_img_lr = model_predict.PredictModelImgLR(path_load_image=path_load_image,
                                                            size_new=512,
                                                            path_load_model=PATH_LOAD_MODEL)
     answer_pred = predict_model_img_lr.predict_model()
 
+    if 'Julia Roberts' in answer_pred:
+        test = True
+    else:
+        test = False
+    print(f'Тест: {test}\n')
     print(answer_pred)
 
 
